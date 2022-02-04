35b6d9c56350d54c3bb99d477f0f174058f8772c
dhanikprakash <dhanikprakash@gmail.com>
2022-02-04 10:56:18 +0000

    update to use fetch directly

--- src/api/searchApi.ts
+++ src/api/searchApi.ts
@@ -1,7 +1,7 @@
 import { handleResponse } from "./apiUtils";
 import {SearchResult} from "../../models/Results";
 
-const baseUrl = "http://localhost:3001/api/";
+const baseUrl = "https://itunes.apple.com/";
 
 export function getResults(offset:number, limit:number, searchKey:string): Promise<SearchResult> {
     return fetch(baseUrl+`search?media=music&offset=${offset}&limit=${limit}&term=${searchKey}`)
