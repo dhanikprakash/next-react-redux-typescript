6b89c9ffa6c3d94fde0bf0165b8b6c732703abd7
dhanikprakash <dhanikprakash@gmail.com>
2022-02-28 12:37:39 +0000

    Updates

---
+++ precache-manifest.b4d0f16971e364c04f77d459d3ce7872.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfc4347d62c5b4899b8980f6e70e33d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2438546c8464bb709562",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "f7371a3b94d135f05ab1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2438546c8464bb709562",
+    "url": "/static/js/2.dfc81f5b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.dfc81f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7371a3b94d135f05ab1",
+    "url": "/static/js/main.269ca2fe.chunk.js"
+  },
+  {
+    "revision": "a84e902bd8e43e523960",
+    "url": "/static/js/runtime-main.f78999d7.js"
+  }
+]);
